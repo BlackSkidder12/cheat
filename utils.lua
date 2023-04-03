36d9ea0fe604dd65f7c3a942a42d648c529e84ce
BlackSkidder12 <106390974+BlackSkidder12@users.noreply.github.com>
2023-04-03 12:46:50 +1000

    Create utils.lua

---
+++ utils.lua
@@ -0,0 +1,73 @@
+local module = {}
+
+module.Players = game:GetService("Players")
+module.ReplicatedStorage = game:GetService("ReplicatedStorage")
+module.TeleportService = game:GetService("TeleportService")
+module.RunService = game:GetService("RunService")
+module.HttpService = game:GetService("HttpService")
+module.CoreGui = game:GetService("CoreGui")
+module.TweenService = game:GetService("TweenService")
+module.PathfindingService = game:GetService("PathfindingService")
+
+module.Player = module.Players.LocalPlayer
+module.Mouse = module.Player:GetMouse()
+module.Character = module.Player.Character
+module.Humanoid = module.Character.Humanoid
+if module.Humanoid ~= nil then
+    module.Health = module.Humanoid.Health
+    module.MaxHealth = module.Humanoid.MaxHealth
+    module.WalkSpeed = module.Humanoid.WalkSpeed
+    module.JumpPower = module.Humanoid.JumpPower
+end
+
+function module.Create(ClassName, Properties)
+    local instance = Instance.new(ClassName)
+
+    for property, value in next, Properties do
+        instance[property] = value
+    end
+    
+    return instance
+end
+
+function Refresh()
+    module.Players = game:GetService("Players")
+    module.ReplicatedStorage = game:GetService("ReplicatedStorage")
+    module.TeleportService = game:GetService("TeleportService")
+    module.RunService = game:GetService("RunService")
+    module.HttpService = game:GetService("HttpService")
+    module.CoreGui = game:GetService("CoreGui")
+    module.TweenService = game:GetService("TweenService")
+    module.PathfindingService = game:GetService("PathfindingService")
+
+    module.Player = module.Players.LocalPlayer
+    module.Mouse = module.Player:GetMouse()
+    module.Character = module.Player.Character
+    module.HumanoidRootPart = module.Character.HumanoidRootPart
+    module.Humanoid = module.Character.Humanoid
+    if module.Humanoid ~= nil then
+        module.Health = module.Humanoid.Health
+        module.MaxHealth = module.Humanoid.MaxHealth
+        module.WalkSpeed = module.Humanoid.WalkSpeed
+        module.JumpPower = module.Humanoid.JumpPower
+    end
+
+    function module.Create(ClassName, Properties)
+        local instance = Instance.new(ClassName)
+
+        for property, value in next, Properties do
+            instance[property] = value
+        end
+        
+        return instance
+    end
+end
+
+Refresh()
+coroutine.wrap(function()
+    while task.wait() do
+        Refresh()
+    end
+end)()
+
+return module
